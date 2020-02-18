d23f707d5890a906936a629a1a766bfe0731e789
Gump Lei <gump.lei@alphasolution.com.mo>
2020-02-18 14:52:49 +0800

    - Update to nodejs 10 - enable eslint and prettier. - update .gitignore

--- {{ projectName }}/webpack.common.js
+++ {{ projectName }}/webpack.common.js
@@ -1,18 +1,31 @@
 const path = require("path");
 
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
-    entry:  './src/index.js',
-    target: 'node',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }]
+  entry: "./src/index.js",
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@": resolve("src")
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [resolve("src"), resolve("test")],
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
